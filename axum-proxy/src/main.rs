8b9ddc5c96be18adbae675bbd2118d6973e881e9
bumzacktrara <bumzacktrara@gmail.com>
2023-03-19 08:37:18 +0100

    axum thingi

--- axum-proxy/src/main.rs
+++ axum-proxy/src/main.rs
@@ -1,3 +1,100 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use std::time::Duration;
+use axum::http::{HeaderMap, Request, StatusCode};
+use axum::{Json, Router};
+use axum::body::Bytes;
+use axum::response::Response;
+use axum::routing::get;
+use axum::routing::post;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use  serde::Deserialize;
+use tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer};
+
+use  serde::Serialize;
+use tracing::Span;
+
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_tracing_aka_logging=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        // `POST /users` goes to `create_user`
+        .route("/users", post(create_user))
+        .layer(TraceLayer::new_for_http())
+        .layer(
+            TraceLayer::new_for_http()
+                .on_request(|_request: &Request<_>, _span: &Span| {
+                   println!("on_request");
+                })
+                .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
+                    println!("on_response");
+                })
+                .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
+                    println!("on_body_chunk");
+                })
+                .on_eos(
+                    |_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {
+                        println!("on_eos");
+                    },
+                )
+                .on_failure(
+                    |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
+                        println!("on_failure");
+                    },
+                ),
+        );
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
