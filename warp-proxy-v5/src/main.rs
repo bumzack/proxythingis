2ca6fdc66bdb8448bf69e7c1b70e4eb7c71a0f26
bumzacktrara <bumzacktrara@gmail.com>
2023-03-20 23:02:49 +0100

    new version which uses streaming body

---
+++ warp-proxy-v5/src/main.rs
@@ -0,0 +1,81 @@
+extern crate lazy_static;
+
+use std::env;
+
+use tokio::sync::mpsc;
+use tracing_subscriber::fmt::format::FmtSpan;
+use warp::Filter;
+use warp::hyper::Client;
+use warp::hyper::client::HttpConnector;
+
+use crate::config_manager::manager::{ProxyConfig, start_config_manager};
+use crate::db::db::create_pool;
+use crate::proxy::route::proxy_routes;
+use crate::proxyserver::db::list_server;
+use crate::proxyserver::route::server_routes;
+use crate::stats::route::stats_routes;
+
+mod db;
+mod config_manager;
+mod proxy;
+mod proxyserver;
+mod server;
+mod stats;
+
+// gotta give credit where credit is due and stuff
+lazy_static::lazy_static! {
+    static ref CLIENT: warp::hyper::Client<HttpConnector> = {
+         let http_connector = HttpConnector::new();
+         let client = Client::builder().build(http_connector);
+         return client;
+    };
+}
+
+
+// #[tokio::main(worker_threads = 2)]
+#[tokio::main]
+async fn main() {
+    let _result = dotenvy::from_filename("/Users/bumzack/stoff/rust/proxythingis/warp-proxy-v4/.env");
+
+    if env::var_os("RUST_LOG").is_none() {
+        // Set `RUST_LOG=todos=debug` to see debug logs,
+        // this only shows access logs.
+        env::set_var("RUST_LOG", "todos=info");
+    }
+    let pool = create_pool();
+
+
+    let servers = list_server(pool.clone(), true).await.expect("loading the servers config should work");
+    let proxy_config = ProxyConfig {
+        server_sources: servers,
+        start: chrono::Utc::now(),
+        stop: chrono::Utc::now(),
+    };
+
+    let (manager_sender, manager_receiver) = mpsc::unbounded_channel();
+
+    let _handle_config_manager = start_config_manager(proxy_config, manager_receiver);
+
+
+    //pretty_env_logger::init();
+    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "tracing=info,warp=debug".to_owned());
+    tracing_subscriber::fmt()
+        // Use the filter we built above to determine which traces to record.
+        .with_env_filter(filter)
+        // Record an event when each span closes. This can be used to time our
+        // routes' durations!
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
+
+    let stats_routes = stats_routes(&pool, &manager_sender);
+    let server_routes = server_routes(pool, &manager_sender);
+    let proxy_routes = proxy_routes(manager_sender);
+
+    let routes = stats_routes
+        .or(server_routes)
+        .or(proxy_routes);
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3035))
+        .await;
+}
---
+++ warp-proxy-v5/src/proxyserver/db.rs
@@ -0,0 +1,208 @@
+use std::collections::HashMap;
+use std::ops::Add;
+
+use chrono::{DateTime, Utc};
+use deadpool_postgres::Pool;
+use tokio_postgres::Row;
+
+use crate::db::db::{TABLE_SOURCE, TABLE_SOURCE2TARGET, TABLE_TARGET};
+use crate::proxyserver::models::{NewServerSourcePost, NewServerTargetPost, Server2Target, ServerSource, ServerSourceStats, ServerTarget, ServerTargetStats};
+use crate::server::models::MyError::DBQueryError;
+use crate::server::server::Result;
+
+impl From<Row> for ServerSource {
+    fn from(value: Row) -> Self {
+        ServerSource {
+            id: value.get(0),
+            description: value.get(1),
+            path_starts_with: value.get(2),
+            method: value.get(3),
+            created: value.get(4),
+            targets: vec![],
+            stats: ServerSourceStats::default(),
+        }
+    }
+}
+
+impl From<Row> for ServerTarget {
+    fn from(value: Row) -> Self {
+        ServerTarget {
+            id: value.get(0),
+            description: value.get(1),
+            schema: value.get(2),
+            host: value.get(3),
+            port: value.get(4),
+            method: value.get(5),
+            path: value.get(6),
+            active: value.get(7),
+            stats: ServerTargetStats::default(),
+            created: Default::default(),
+        }
+    }
+}
+
+impl From<Row> for Server2Target {
+    fn from(value: Row) -> Self {
+        Server2Target {
+            id: value.get(0),
+            source_id: value.get(1),
+            target_id: value.get(2),
+        }
+    }
+}
+
+
+pub async fn create_source(pool: Pool, body: NewServerSourcePost) -> Result<ServerSource> {
+    let client = pool.get().await.unwrap();
+    let query = format!("INSERT INTO {} (description, path_starts_with, method) VALUES ($1, $2, $3) RETURNING *", TABLE_SOURCE);
+    // println!("new server source {:?}", &body);
+    // println!("query   {}", &query);
+    let row = client
+        .query_one(query.as_str(), &[&body.description, &body.path_starts_with, &body.method])
+        .await
+        .map_err(DBQueryError)?;
+    let server_source = ServerSource::from(row);
+    Ok(server_source)
+}
+
+pub async fn create_target(pool: Pool, body: NewServerTargetPost) -> Result<ServerTarget> {
+    let client = pool.get().await.unwrap();
+    let query = format!("INSERT INTO {} (description, schema, host, port, path, method, active) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *", TABLE_TARGET);
+    // println!("new server target {:?}", &body);
+    // println!("query   {}", &query);
+    let row = client
+        .query_one(query.as_str(), &[&body.description, &body.schema, &body.host, &body.port, &body.path, &body.method, &body.active])
+        .await
+        .map_err(DBQueryError)?;
+    let server_target = ServerTarget::from(row);
+
+    // add cross table entry
+    let _source_2_target = create_source2target(pool.clone(), body.source, server_target.id).await.unwrap();
+    // println!("source_2_target   {:?}", &source_2_target);
+
+    Ok(server_target)
+}
+
+pub async fn create_source2target(pool: Pool, source_id: i32, target_id: i32) -> Result<Server2Target> {
+    let client = pool.get().await.unwrap();
+    let query = format!("INSERT INTO {} (source_id, target_id) VALUES ($1, $2) RETURNING *", TABLE_SOURCE2TARGET);
+    // println!("new source -> target  {:?} -> {:?}", source_id, target_id);
+    // println!("query   {}", &query);
+    let row = client
+        .query_one(query.as_str(), &[&source_id, &target_id])
+        .await
+        .map_err(DBQueryError)?;
+    let server_source = Server2Target::from(row);
+    Ok(server_source)
+}
+
+pub async fn list_server(pool: Pool, active_only: bool) -> Result<Vec<ServerSource>> {
+    let client = pool.get().await.unwrap();
+    // source server
+
+    let query1 = format!("SELECT {}.id AS source_id, {}.description AS source_description, {}.path_starts_with AS source_path_starts_with , {}.method AS source_method , {}.created AS source_created, ", TABLE_SOURCE, TABLE_SOURCE, TABLE_SOURCE, TABLE_SOURCE, TABLE_SOURCE);
+    let query2 = format!("{}.id AS target_id, {}.description AS target_description,   {}.schema AS target_schema,  {}.host AS target_host,  {}.port AS target_port ,  {}.path AS target_path ,  {}.method AS target_method ,  {}.active AS target_active ,{}.created AS target_created      FROM {} ", TABLE_TARGET, TABLE_TARGET, TABLE_TARGET, TABLE_TARGET, TABLE_TARGET, TABLE_TARGET, TABLE_TARGET, TABLE_TARGET, TABLE_TARGET, TABLE_SOURCE2TARGET);
+    let query3 = format!(" LEFT JOIN {} ON ({}.source_id = {}.id) ", TABLE_SOURCE, TABLE_SOURCE2TARGET, TABLE_SOURCE);
+    let query4 = format!(" LEFT JOIN {} ON ({}.target_id = {}.id) ", TABLE_TARGET, TABLE_SOURCE2TARGET, TABLE_TARGET);
+
+    // println!("query1 {}", &query1);
+    // println!("query2 {}", &query2);
+    // println!("query3 {}", &query3);
+    // println!("query4 {}", &query4);
+
+    let mut map: HashMap<i32, ServerSource> = HashMap::new();
+
+    let query_full = query1.add(&query2).add(&query3).add(&query4);
+    // println!("query   {}", &query_full);
+    let data = client.query(&query_full, &[]).await.unwrap();
+    for row in data {
+        let source_id: i32 = row.get("source_id");
+        let source_description: &str = row.get("source_description");
+        let source_path_starts_with: &str = row.get("source_path_starts_with");
+        let source_method: &str = row.get("source_method");
+        let source_created: DateTime<Utc> = row.get("source_created");
+
+        let target_id: i32 = row.get("target_id");
+        let target_description: &str = row.get("target_description");
+        let target_schema: &str = row.get("target_schema");
+        let target_host: &str = row.get("target_host");
+        let target_port: i32 = row.get("target_port");
+        let target_path: &str = row.get("target_path");
+        let target_method: &str = row.get("target_method");
+        let target_active: bool = row.get("target_active");
+        let target_created: DateTime<Utc> = row.get("target_created");
+
+
+        // println!("found server source: {} {} {} {:?}", source_id, source_description, source_method, source_path_starts_with);
+        // println!("\tfound server target: {} {} {} {:?} {} {} {:?}", target_id, target_description, target_schema, target_port, target_path, target_method, target_active);
+
+
+        let mut server_source = ServerSource {
+            id: source_id,
+            description: source_description.to_string(),
+            path_starts_with: source_path_starts_with.to_string(),
+            method: source_method.to_string(),
+            created: source_created,
+            targets: vec![],
+            stats: Default::default(),
+        };
+
+        let server_target = ServerTarget {
+            id: target_id,
+            description: target_description.to_string(),
+            schema: target_schema.to_string(),
+            host: target_host.to_string(),
+            port: target_port,
+            path: target_path.to_string(),
+            method: target_method.to_string(),
+            active: target_active,
+            stats: Default::default(),
+            created: target_created,
+        };
+
+        if active_only {
+            if server_target.active {
+                add_to_map(&mut map, &mut server_source, server_target);
+            }
+        } else {
+            add_to_map(&mut map, &mut server_source, server_target);
+        }
+    }
+
+    let sources: Vec<ServerSource> = map.values()
+        .cloned()
+        .collect();
+
+    Ok(sources)
+}
+
+fn add_to_map(map: &mut HashMap<i32, ServerSource>, server_source: &ServerSource, server_target: ServerTarget) {
+    if map.contains_key(&server_source.id) {
+        let s = map.get_mut(&server_source.id).unwrap();
+        s.targets.push(server_target);
+    } else {
+        let mut server_source = server_source.clone();
+        server_source.targets.push(server_target);
+        map.insert(server_source.id, server_source);
+    }
+}
+
+pub async fn activate_server(pool: Pool, id: i32) -> Result<()> {
+    change_activate_server(pool, id, true).await
+}
+
+pub async fn deactivate_server(pool: Pool, id: i32) -> Result<()> {
+    change_activate_server(pool, id, false).await
+}
+
+pub async fn change_activate_server(pool: Pool, id: i32, val: bool) -> Result<()> {
+    println!("(de-)activating server {}. val {}", id, val);
+    let client = pool.get().await.unwrap();
+    let query = format!("UPDATE  {}  SET active= {} WHERE  id = $1 RETURNING *", TABLE_TARGET, val);
+
+    let _row = client
+        .query_one(query.as_str(), &[&id])
+        .await
+        .map_err(DBQueryError)?;
+    Ok(())
+}
