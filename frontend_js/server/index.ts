5abd4030a2b7286683f9e14e234c6c650f597aeb
bumzacktrara <bumzacktrara@gmail.com>
2023-03-22 22:38:38 +0100

    dummy front end for CORS tests

---
+++ frontend_js/server/index.ts
@@ -0,0 +1,22 @@
+import * as Koa from "koa";
+import * as serve from "koa-static";
+import * as cors from "@koa/cors";
+import * as path from "path";
+import * as dotenv from 'dotenv'
+
+export {}
+
+dotenv.config()
+
+const port = process.env.PORT || 5000
+
+
+const app = new Koa()
+app.use(cors());
+const fe_app_dir = '../fe/dist';
+const fe_app_path = path.join(__dirname, fe_app_dir);
+console.log("path ", fe_app_path);
+app.use(serve(fe_app_path))
+
+console.log("searchapp: " + fe_app_path+ " on port: "+ port);
+app.listen(port)
