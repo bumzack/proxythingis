f426e8a5af896713eb37e5d07272fc85caaec194
bumzacktrara <bumzacktrara@gmail.com>
2023-05-07 00:33:43 +0200

    common

---
+++ proxythingi/common/src/models.rs
@@ -0,0 +1,112 @@
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Deserialize, Serialize, Clone, Debug, Default, PartialEq)]
+// #[cfg_attr(feature = "sycamore_support", derive(Props))]
+pub struct ServerSource {
+    pub id: i32,
+    pub description: String,
+    pub path_starts_with: String,
+    pub method: String,
+    pub created: DateTime<Utc>,
+    pub targets: Vec<ServerTarget>,
+    pub stats: ServerSourceStats,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, Default, PartialEq)]
+// #[cfg_attr(feature = "sycamore_support", derive(Props))]
+pub struct ServerTarget {
+    pub id: i32,
+    pub description: String,
+    pub schema: String,
+    pub host: String,
+    pub port: i32,
+    pub path: String,
+    pub method: String,
+    pub stats: ServerTargetStats,
+    pub active: bool,
+    pub created: DateTime<Utc>,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, Default, PartialEq)]
+pub struct Server2Target {
+    pub id: i32,
+    pub source_id: i32,
+    pub target_id: i32,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, Default, PartialEq)]
+pub struct NewServerSourcePost {
+    pub description: String,
+    pub path_starts_with: String,
+    pub method: String,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, Default, PartialEq)]
+pub struct NewServerSource<'a> {
+    pub description: &'a str,
+    pub path_starts_with: &'a str,
+    pub method: &'a str,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, Default, PartialEq)]
+pub struct NewServerTargetPost {
+    pub description: String,
+    pub schema: String,
+    pub host: String,
+    pub port: i32,
+    pub path: String,
+    pub method: String,
+    pub source: i32,
+    pub active: bool,
+}
+
+#[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
+pub struct NewServerTarget<'a> {
+    pub description: &'a str,
+    pub schema: &'a str,
+    pub host: &'a str,
+    pub port: i32,
+    pub path: &'a str,
+    pub method: &'a str,
+    pub source: i32,
+    pub active: bool,
+}
+
+#[derive(Serialize)]
+pub struct ErrorResponse {
+    pub message: String,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, Default, PartialEq)]
+// #[cfg_attr(feature = "sycamore_support", derive(Props))]
+pub struct ServerSourceStats {
+    pub id: i32,
+    pub source_id: i32,
+    pub hits: i128,
+    pub start: DateTime<Utc>,
+    pub stop: DateTime<Utc>,
+    pub created: DateTime<Utc>,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, Default, PartialEq)]
+// #[cfg_attr(feature = "sycamore_support", derive(Props))]
+pub struct ServerTargetStats {
+    pub id: i32,
+    pub target_id: i32,
+    pub hits: i128,
+    pub avg_ns: i128,
+    pub max_ns: i128,
+    pub min_ns: i128,
+    pub start: DateTime<Utc>,
+    pub stop: DateTime<Utc>,
+    pub created: DateTime<Utc>,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct ProxyConfig {
+    pub server_sources: Vec<ServerSource>,
+    pub start: DateTime<Utc>,
+    pub stop: DateTime<Utc>,
+}
---
+++ proxythingi/common/src/warp_server.rs
@@ -0,0 +1,89 @@
+pub mod warp_request_filter {
+    use warp::http::HeaderMap;
+    use warp::hyper::body::Bytes;
+    use warp::hyper::Method as RequestMethod;
+    use warp::path::FullPath;
+    use warp::{Buf, Filter, Stream};
+
+    pub type ProxyQueryParameters = Option<String>;
+    pub type ProxyHeaders = HeaderMap;
+    pub type ProxyMethod = RequestMethod;
+    pub type ProxyRequest = (
+        ProxyUri,
+        ProxyQueryParameters,
+        ProxyMethod,
+        ProxyHeaders,
+        Bytes,
+    );
+    pub type ProxyUri = FullPath;
+    pub type ProxyRequestBodyAsString = (
+        ProxyUri,
+        ProxyQueryParameters,
+        ProxyMethod,
+        ProxyHeaders,
+        String,
+    );
+
+    pub fn query_params_filter(
+    ) -> impl Filter<Extract = (ProxyQueryParameters,), Error = std::convert::Infallible> + Clone
+    {
+        warp::query::raw()
+            .map(Some)
+            .or_else(|_| async { Ok::<(ProxyQueryParameters,), std::convert::Infallible>((None,)) })
+    }
+
+    pub fn extract_request_data_filter(
+    ) -> impl Filter<Extract = ProxyRequest, Error = warp::Rejection> + Clone {
+        warp::path::full()
+            .and(query_params_filter())
+            .and(warp::method())
+            .and(warp::header::headers_cloned())
+            .and(warp::body::bytes())
+    }
+
+    pub fn extract_request_data_filter_body_stream() -> impl Filter<
+        Extract = (
+            ProxyUri,
+            ProxyQueryParameters,
+            ProxyMethod,
+            ProxyHeaders,
+            impl Stream<Item = Result<impl Buf, warp::Error>> + Send + 'static,
+        ),
+        Error = warp::Rejection,
+    > + Clone {
+        warp::path::full()
+            .and(query_params_filter())
+            .and(warp::method())
+            .and(warp::header::headers_cloned())
+            .and(warp::body::stream())
+    }
+
+    pub fn extract_request_data_filter_body_as_string(
+    ) -> impl Filter<Extract = ProxyRequestBodyAsString, Error = warp::Rejection> + Clone {
+        warp::path::full()
+            .and(query_params_filter())
+            .and(warp::method())
+            .and(warp::header::headers_cloned())
+            .and(string_filter())
+    }
+
+    // https://github.com/seanmonstar/warp/issues/248
+
+    /// Extracts the body of a request as string
+    pub fn string_filter() -> impl Filter<Extract = (String,), Error = warp::Rejection> + Clone {
+        warp::filters::body::bytes().and_then(convert_to_string)
+    }
+
+    async fn convert_to_string(bytes: Bytes) -> Result<String, warp::Rejection> {
+        match String::from_utf8(bytes.to_vec()) {
+            Ok(b) => Ok(b),
+            Err(_) => Ok(String::default()),
+        }
+    }
+
+    pub const HEADER_X_INITIATED_BY: &str = "X-initiated-by";
+
+    pub const HEADER_X_PROCESSED_BY: &str = "X-processed-by";
+
+    pub const HEADER_X_UUID: &str = "X-uuid";
+}
